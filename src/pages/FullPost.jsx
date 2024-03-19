23e39c7d9a03c95831a7063cbbad275fd7ee2ec3
Roman <157926620+roman0rama@users.noreply.github.com>
2024-03-19 10:43:35 +0300

    Add files via upload

---
+++ src/pages/FullPost.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+
+import { useParams } from 'react-router-dom'
+import { Post } from "../components/Post";
+import { Index } from "../components/AddComment";
+import { CommentsBlock } from "../components/CommentsBlock";
+import axios from "../axios";
+
+export const FullPost = () => {
+    const [ data, setData ] = React.useState()
+    const [ isLoading, setIsLoading ] = React.useState(true)
+    const { id } = useParams()
+
+    React.useEffect(() => {
+        axios.get(`/posts/${id}`).then(res => {
+            setData(res.data)
+            setIsLoading(false)
+        }).catch(err => {
+            console.warn(err)
+            alert('Ошибка при получении статьи. Обратитесь к администрации')
+        })
+    }, [])
+
+    if (isLoading) {
+        return (<Post isLoading={isLoading} isFullPost/>)
+    }
+
+  return (
+    <>
+      <Post
+          _id={data._id}
+          title={data.title}
+          imageUrl={data.imageUrl}
+          //imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
+          user={data.user}
+          createdAt={data.createdAt}
+          viewsCount={data.viewCount}
+          commentsCount={3}
+          tags={data.tags}
+        isFullPost
+      >
+        <p>
+            {data.text}
+        </p>
+      </Post>
+      <CommentsBlock
+        items={[
+          {
+            user: {
+              fullName: "Вася Пупкин",
+              avatarUrl: "https://mui.com/static/images/avatar/1.jpg",
+            },
+            text: "Это тестовый комментарий 555555",
+          },
+          {
+            user: {
+              fullName: "Иван Иванов",
+              avatarUrl: "https://mui.com/static/images/avatar/2.jpg",
+            },
+            text: "When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top",
+          },
+        ]}
+        isLoading={false}
+      >
+        <Index />
+      </CommentsBlock>
+    </>
+  );
+};
